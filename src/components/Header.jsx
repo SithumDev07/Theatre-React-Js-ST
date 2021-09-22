39b5571591a4cc052ca28a1f14bfe6a5a4fdc4d0
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-09-22 22:42:06 +0530

    Added Rating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
         <div className='w-full h-auto py-16'>
             <div className='w-3/4 h-full flex flex-col mx-auto mt-10'>
                 <h3 className='ml-1 uppercase tracking-widest font-bold text-red-700' style={{ userSelect: 'none' }}>Now Playing</h3>
-                <h1 className='text-gray-50 text-8xl uppercase font-light'><span className='font-extrabold tracking-wide' style={{ userSelect: 'none' }}>Now</span> Playing</h1>
+                <h1 className='text-gray-50 text-8xl uppercase font-light' style={{ userSelect: 'none' }}><span className='font-extrabold tracking-wide'>Now</span> Playing</h1>
                 <button className='ml-1 uppercase flex items-center mt-8 bg-red-500 px-3 py-3 self-start text-gray-100 font-semibold tracking-wide'>
                     <FilmIcon className='w-6 h-6 mr-2' />
                     View Showtimes
--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { StarIcon } from '@heroicons/react/solid'
 
 function MovieItem({ title, imageURI, duration, releasedDate, rating, genre }) {
     return (
-        <div className='cursor-pointer'>
-            {/* https://i.redd.it/x98yj7ti6h831.jpg */}
+        <div className='cursor-pointer relative'>
+            <div className='flex items-center absolute top-1 left-2 text-gray-50 font-bold z-10 text-xl'>
+                {rating}
+                <StarIcon className='ml-1 w-4 h-4 text-yellow-500' />
+            </div>
             <div className='h-80 w-64'>
                 <img src={imageURI} className='object-cover object-bottom w-full h-full hover:opacity-75 transition ease-in-out duration-150' alt="Image" />
             </div>
             <h2 className='text-gray-50 font-extrabold mt-2 text-xl'>{title}</h2>
             <p className='text-gray-50 my-3 text-sm'>{duration} | {genre.map((item) => `${item}, `)}</p>
             <p className='text-gray-50 text-sm'>Released {releasedDate}</p>
-            <button className='border-opacity-70 border-2 border-red-600 px-7 py-2 uppercase text-gray-50 tracking-wide font-semibold mt-3'>Get Tickets</button>
+            <button className='border-opacity-70 border-2 border-red-600 px-7 py-2 uppercase text-gray-50 tracking-wide font-semibold mt-3 hover:bg-red-500 transform transition duration-300'>Get Tickets</button>
         </div>
     )
 }
